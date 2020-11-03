a13ec159fd7860259ff60f168cfded1c167b8891
Alexey <37597550+Borshecs@users.noreply.github.com>
2020-11-03 19:14:09 +0300

    кусь!!!

---
+++ Закр квадрат.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 20;
+	int b = 20;
+	for (int i = 1; i < a; i++)
+	{
+		for (int j = 1; j < b; j++)
+		{
+			cout << "@";
+		}
+		cout << endl;
+	}
+	return 0;
+}
