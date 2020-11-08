b32bdb680a1894d1ab58c183d421ef721b9b229e
Alexey <37597550+Borshecs@users.noreply.github.com>
2020-11-08 20:53:04 +0300

    TaskB рабоает урааа

--- Функция.cpp
+++ Функция.cpp
@@ -1,7 +1,12 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
 #include <cmath>
 #include <string>
 
+double calc(double a,  double x);
+double calc(double x);
+void taskA(double a, double xn, double xk, double dx);
+void taskB(double a,  double* x, int size);
 
 using namespace std;
 
@@ -10,15 +15,18 @@ double calc(double a, double x)
 	double y = pow(a, x * x - 1) - log(x * x - 1) + pow(x * x - 1, 1.0 / 3.0);
 	return y;
 }
+
+
 double calc(double x)
 {
 	const double a = 2.25;
+
 	return calc(a, x);
 }
 
 void taskA(double a, double xn, double xk, double dx)
 {
-	for (double x = xn; x <= xk; x = x + dx)
+	for (double x = xn; x <= xk; x += dx)
 	{
 		double y = calc(a, x);
 		cout << "x = " << x << "\t" << "y = " << y << std::endl;
@@ -26,45 +34,32 @@ void taskA(double a, double xn, double xk, double dx)
 	return;
 }
 
-void taskB(double a, double *x, int size) // TaskB выводит немного не то, что нужно, почему?
+void taskB(double a, double* x, int size)
 {
 	for (int i = 0; i < size; i++)
 	{
-		double x[] = { 1.31, 1.39, 1.44, 1.56, 1.92 };
 		double y = calc(a, x[i]);
 		char result[80];
 		sprintf_s(result, "x = %3.5f y = %.2f \n", x[i], y);
 		cout << result << endl;
-		return;
+
 	}
 }
 int main(int argc, char* argv[])
 {
-	/*int a = 2.25;
-	int xn = 1.2;
-	int xk = 2.7;
-	int deltax = 0.3;
-	*/
+	double a = 2.25;
+	double b = 0.95;
+	double xn = 1.2;
+	double xk = 2.7;
+	double dx = 0.3;
+	cout << "Variant 22" << endl;
+	cout << "taskA:" << endl;
+	taskA(a, xn, xk, dx);
 
-	const double a = 2.25;
-	cout << "TaskA:" << endl;
-	taskA(2.25, 1.2, 2.7, 0.3);
 	double x[] = { 1.31, 1.39, 1.44, 1.56, 1.92 };
-	cout << "TaskB:" << endl;
-	taskB(a, x, sizeof(x) / sizeof(x[0]));
+	cout << "taskB:" << endl;
+	taskB(2.25,  x, sizeof(x) / sizeof(x[0]));
+
 	return 0;
-
 }
-
-
-
-
-
-/*
-double* X = new double[4]{ };
-B( );
-delete[] X;
-return 0;
-*/
-
 
